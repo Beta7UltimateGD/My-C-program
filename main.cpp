f85b2193bcd745e1de8734c38fd5fd68f1cab6f8
Beta7UltimateGD <viethoang082013principal@gmail.com>
2025-01-04 20:39:55 +0700

    Update main.cpp
    not done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
 
+#include <cmath>
+
+int main()
+{
+  std::string CONTROLLER_OPERATIONS;
+  std::string SHIFT;
+  std::string ALPHA;
+  std::string UP;
+  std::string MODE;
+  std::string SETUP;
+  std::string ON;
+  std::cout << "SHIFT         " << "ALPHA          " << "^" << "           MODE/SETUP            " << "ON" << endl;
+  switch(CONTROLLER_OPERATIONS){
+    case "SHIFT":
+      std::cin >> SHIFT;
+    	std::cout << "S";
+  }
+}
